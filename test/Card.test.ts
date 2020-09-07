a3bb1814fefd341b7cfdde5837f70facacaa1bdd
Ilia Feldgun <iliafeldgun@outlook.com>
2020-09-07 18:57:14 +0300

    Added tests for card

--- test/Card.test.ts
+++ test/Card.test.ts
@@ -11,4 +11,34 @@ describe('Testing card model', () => {
         }
         expect(card).toMatchObject(cardMock)
     })
+    
+    test('Testing isCard check', () => {
+        const card = new Card(Suit.HEART, AceHighRank.QUEEN)
+        const card2 = {suit: Suit.DIAMOND, rank: AceHighRank.EIGHT}
+        const badCard = {suit: Suit.SPADE, rank: AceHighRank.FIVE, bad: "okay"}
+        const badCard2 = {suit: 200, rank: AceHighRank.FOUR}
+        const badCard3 = {suit: Suit.CLUB, rank: -12}
+        const badCard4 = {suit: -11, rank: AceHighRank.SIX}
+        const badCard5 = {suit: Suit.HEART, rank: 500}
+        expect(Card.isCard(card)).toBe(true)
+        expect(Card.isCard(card2)).toBe(true)
+        expect(Card.isCard(badCard)).toBe(false)
+        expect(Card.isCard(badCard2)).toBe(false)
+        expect(Card.isCard(badCard3)).toBe(false)
+        expect(Card.isCard(badCard4)).toBe(false)
+        expect(Card.isCard(badCard5)).toBe(false)
+    })
+
+    test('Testing equals check', () => {
+        const card = new Card(Suit.DIAMOND, AceHighRank.KING)
+        const cardAgain = {suit: Suit.DIAMOND, rank: AceHighRank.KING}
+        const cardSameSuit = new Card(Suit.DIAMOND, AceHighRank.JACK)
+        const cardSameRank = new Card(Suit.SPADE, AceHighRank.KING)
+        const cardOther = new Card(Suit.CLUB, AceHighRank.JOKER)
+
+        expect(Card.equals(card, cardAgain)).toBe(true)
+        expect(Card.equals(card, cardSameSuit)).toBe(false)
+        expect(Card.equals(card, cardSameRank)).toBe(false)
+        expect(Card.equals(card, cardOther)).toBe(false)
+    })
 })
