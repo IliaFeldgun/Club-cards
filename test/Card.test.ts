f10d3478c95edcb4bfee4572e518aa7a9f0263e6
Ilia Feldgun <iliafeldgun@outlook.com>
2020-09-07 19:18:35 +0300

    Card getColor added

--- test/Card.test.ts
+++ test/Card.test.ts
@@ -1,6 +1,7 @@
 import Card from '../src/card_engine/models/Card'
+import { Color } from '../src/card_engine/enums/Color'
 import { Suit } from '../src/card_engine/enums/Suit'
-import { AceHighRank } from '../src/card_engine/enums/Rank'
+import { AceHighRank, AceLowRank } from '../src/card_engine/enums/Rank'
 
 describe('Testing card model', () => {
     test('Testing card creation', () => {
@@ -11,15 +12,15 @@ describe('Testing card model', () => {
         }
         expect(card).toMatchObject(cardMock)
     })
-    
+
     test('Testing isCard check', () => {
         const card = new Card(Suit.HEART, AceHighRank.QUEEN)
-        const card2 = {suit: Suit.DIAMOND, rank: AceHighRank.EIGHT}
-        const badCard = {suit: Suit.SPADE, rank: AceHighRank.FIVE, bad: "okay"}
-        const badCard2 = {suit: 200, rank: AceHighRank.FOUR}
-        const badCard3 = {suit: Suit.CLUB, rank: -12}
-        const badCard4 = {suit: -11, rank: AceHighRank.SIX}
-        const badCard5 = {suit: Suit.HEART, rank: 500}
+        const card2 = { suit: Suit.DIAMOND, rank: AceHighRank.EIGHT }
+        const badCard = { suit: Suit.SPADE, rank: AceHighRank.FIVE, bad: "okay" }
+        const badCard2 = { suit: 200, rank: AceHighRank.FOUR }
+        const badCard3 = { suit: Suit.CLUB, rank: -12 }
+        const badCard4 = { suit: -11, rank: AceHighRank.SIX }
+        const badCard5 = { suit: Suit.HEART, rank: 500 }
         expect(Card.isCard(card)).toBe(true)
         expect(Card.isCard(card2)).toBe(true)
         expect(Card.isCard(badCard)).toBe(false)
@@ -31,7 +32,7 @@ describe('Testing card model', () => {
 
     test('Testing equals check', () => {
         const card = new Card(Suit.DIAMOND, AceHighRank.KING)
-        const cardAgain = {suit: Suit.DIAMOND, rank: AceHighRank.KING}
+        const cardAgain = { suit: Suit.DIAMOND, rank: AceHighRank.KING }
         const cardSameSuit = new Card(Suit.DIAMOND, AceHighRank.JACK)
         const cardSameRank = new Card(Suit.SPADE, AceHighRank.KING)
         const cardOther = new Card(Suit.CLUB, AceHighRank.JOKER)
@@ -40,5 +41,17 @@ describe('Testing card model', () => {
         expect(Card.equals(card, cardSameSuit)).toBe(false)
         expect(Card.equals(card, cardSameRank)).toBe(false)
         expect(Card.equals(card, cardOther)).toBe(false)
+    })
+
+    test('Testing get color check', () => {
+        const cardDiamond = new Card(Suit.DIAMOND, AceLowRank.KING)
+        const cardHeart = new Card(Suit.HEART, AceLowRank.JOKER)
+        const cardClub = new Card(Suit.CLUB, AceLowRank.TEN)
+        const cardSpade = new Card(Suit.SPADE, AceLowRank.FIVE)
+
+        expect(Card.getColor(cardDiamond)).toBe(Color.RED)
+        expect(Card.getColor(cardHeart)).toBe(Color.RED)
+        expect(Card.getColor(cardClub)).toBe(Color.BLACK)
+        expect(Card.getColor(cardSpade)).toBe(Color.BLACK)
     })
 })
